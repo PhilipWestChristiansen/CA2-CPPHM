e4caf2336c0480601d6af71bbd6d53eaaf33d6ef
Hamza Laroussi <hamzaconnection@hotmail.com>
2016-10-05 12:54:13 +0200

    no message

--- src/main/java/Rest/ApplicationConfig.java
+++ src/main/java/Rest/ApplicationConfig.java
@@ -32,6 +32,7 @@ public class ApplicationConfig extends Application
      */
     private void addRestResourceClasses(Set<Class<?>> resources)
     {
+        resources.add(Rest.GenericResource.class);
         resources.add(Rest.RESTPerson.class);
     }
     
--- target/generated-sources/annotations/Entity/CityInfo_.java
+++ target/generated-sources/annotations/Entity/CityInfo_.java
@@ -1,18 +1,14 @@
 package Entity;
 
-import Entity.Address;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-04T13:56:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-05T12:42:10")
 @StaticMetamodel(CityInfo.class)
 public class CityInfo_ { 
 
-    public static volatile SingularAttribute<CityInfo, String> zipcode;
-    public static volatile ListAttribute<CityInfo, Address> addresses;
+    public static volatile SingularAttribute<CityInfo, String> zip;
     public static volatile SingularAttribute<CityInfo, String> city;
-    public static volatile SingularAttribute<CityInfo, Integer> id;
 
 }
