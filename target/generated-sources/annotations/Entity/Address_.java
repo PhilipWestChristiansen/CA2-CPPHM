073541f74f183409ac3bd77473379eeb76a13632
Hamza Laroussi <hamzaconnection@hotmail.com>
2016-10-06 12:28:41 +0200

    Mathias here you go :)

--- target/generated-sources/annotations/Entity/Address_.java
+++ target/generated-sources/annotations/Entity/Address_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-05T16:58:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-06T12:23:26")
 @StaticMetamodel(Address.class)
 public class Address_ { 
 
