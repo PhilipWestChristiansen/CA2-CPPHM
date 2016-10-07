4a730b2dfc6701e148bb413fe69b223c34d6f291
DELL-E4310\Mathias <dudi.achmed@yahoo.dk>
2016-10-07 14:04:27 +0200

    Lavet addperson(POST) - virker ikke

--- src/main/java/Rest/RESTPerson.java
+++ src/main/java/Rest/RESTPerson.java
@@ -21,6 +21,7 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
@@ -67,23 +68,23 @@ public class RESTPerson
   {
     String jsonString = "[";
     List<Person> list = pf.getPersons();
-    
+
     for (int i = 1; i < list.size() + 1; i++)
     {
       Person p = pf.getPerson(i);
       PersonMapper map = new PersonMapper(p);
 
       jsonString += new Gson().toJson(map);
-      
-      if(i != list.size())
+
+      if (i != list.size())
       {
         jsonString += ",";
       }
-      
+
       jsonString += "\n";
 
     }
-    
+
     jsonString += "]";
 
     return jsonString;
@@ -100,14 +101,15 @@ public class RESTPerson
 
   }
 
-  /**
-   * PUT method for updating or creating an instance of GenericResource
-   *
-   * @param content representation for the resource
-   */
-  @PUT
-  @Consumes(MediaType.APPLICATION_XML)
-  public void putXml(String content)
+  @POST
+  //@Produces(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public String postJson(String content)
   {
+    
+    Person p = pf.addPerson(new Gson().fromJson(content, Person.class));
+    
+    return new Gson().toJson(p);
   }
 }
